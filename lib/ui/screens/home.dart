bffc5e9896744137823f5b6adac0c1282982ce70
Nishant <nishantyt0@gmail.com>
2022-11-02 14:31:55 +0545

    updated home page

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:agrify/ui/utilities/colors.dart';
+import 'package:agrify/ui/utilities/constant.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,17 +8,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimarySwatch,
+        title: Row(
+          children: [Text('Agrify')],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+          CircleAvatar(),
+          spacer(width: 20)
+        ],
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  color: kPrimarySwatch,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                spacer(height: 25),
+                const Text(
+                  'Dashboard',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
                 ),
-                child: Text('agrify - a revolution'.toUpperCase()),
-              )
-            ],
+                spacer(height: 10),
+                const Text(
+                  'Welcome Nishant Pokhrel!',
+                  style: TextStyle(fontSize: 16),
+                ),
+                spacer(height: 20),
+                Container(
+                  padding: const EdgeInsets.only(left: 20),
+                  decoration: BoxDecoration(color: Colors.black.withOpacity(0.1)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Weather Forecast', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),),
+                      IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_forward_ios_rounded),),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
