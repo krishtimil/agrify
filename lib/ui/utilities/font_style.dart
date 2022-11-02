31aa2ca16598e28d8e4a4dd1d4d2f47a9c10a189
Nishant <nishantyt0@gmail.com>
2022-11-02 13:00:30 +0545

    added general utilities

---
+++ lib/ui/utilities/font_style.dart
@@ -0,0 +1,33 @@
+import 'package:agrify/ui/utilities/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final TextTheme textTheme = TextTheme(
+  headline1: GoogleFonts.poppins(
+      fontSize: 97, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+  headline2: GoogleFonts.poppins(
+      fontSize: 61, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+  headline3: GoogleFonts.poppins(fontSize: 48, fontWeight: FontWeight.w400),
+  headline4: GoogleFonts.poppins(
+      fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  headline5: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w400),
+  headline6: GoogleFonts.poppins(
+      fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+  subtitle1: GoogleFonts.poppins(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+  subtitle2: GoogleFonts.poppins(
+      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+  bodyText1: GoogleFonts.poppins(
+      fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+  bodyText2: GoogleFonts.poppins(
+      fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+  button: GoogleFonts.poppins(
+      fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+  caption: GoogleFonts.poppins(
+      fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+  overline: GoogleFonts.poppins(
+      fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+);
+
+TextStyle kWhiteHeading = const TextStyle(
+    fontSize: 24, fontWeight: FontWeight.w600, color: kWhiteColor);
