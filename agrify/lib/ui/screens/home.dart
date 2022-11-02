ad7df6e1a3a6a2e294c76ffff659dc5318f13070
Nishant <nishantyt0@gmail.com>
2022-11-02 20:13:20 +0545

    implemented bottom bar functionally

--- agrify/lib/ui/screens/home.dart
+++ agrify/lib/ui/screens/home.dart
@@ -6,17 +6,43 @@ import 'package:flutter/material.dart';
 import '../utilities/colors.dart';
 import '../utilities/constant.dart';
 
-class MyHome extends StatelessWidget {
+class MyHome extends StatefulWidget {
   const MyHome({super.key});
 
   @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  bool isLoggedIn = AuthMethods().authState;
+  int currentIndex = 0;
+
+  Widget setBottombarPage() {
+    switch (currentIndex) {
+      case 0:
+        return HomePage(isLoggedIn: isLoggedIn);
+        break;
+      case 1:
+        return Container( child: Text('Cart'),);
+      case 2:
+        return Container(child: Text('Weather'),);
+      default:
+    }
+    return Container();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    bool isLoggedIn = AuthMethods().authState;
-    
     return Scaffold(
       backgroundColor: kPrimarySwatch,
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
         items: [
           BottomNavigationBarItem(
               icon: Icon(
@@ -45,222 +71,237 @@ class MyHome extends StatelessWidget {
         ],
       ),
       body: SafeArea(
-        child: Column(children: [
-          // grettings row
-          spacer(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Column(
+        child: setBottombarPage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+    required this.isLoggedIn,
+  }) : super(key: key);
+
+  final bool isLoggedIn;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      // grettings row
+      spacer(height: 20),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // Hi, Jared!
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Hi, ${isLoggedIn?  "Nishant": "Agriculturist!"}',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8,
-                        ),
-                        Text(
-                          '23 Jan, 2021',
-                          style: TextStyle(
-                            color: kPrimarySwatch.shade100,
-                          ),
-                        )
-                      ],
-                    ),
-                    //Notification
-                    Container(
-                      decoration: BoxDecoration(
-                        //color: kPrimarySwatch.shade100,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      padding: EdgeInsets.all(12),
-                      child: isLoggedIn
-                          ? CircleAvatar()
-                          : IconButton(
-                              onPressed: () async {
-                                Navigator.pushReplacementNamed(
-                                    context, '/login');
-                              },
-                              icon: const Icon(
-                                Icons.login,
-                                color: Colors.white,
-                              ),
-                            ),
-                    )
-                  ],
-                ),
-
-                SizedBox(
-                  height: 28,
-                ),
-
-                //search bar
-                Container(
-                  decoration: BoxDecoration(
-                    color: kPrimarySwatch.shade100,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  padding: EdgeInsets.all(12),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.search,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'Search',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                SizedBox(
-                  height: 25,
-                ),
-
-                // weather
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // Hi, Jared!
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Weather Updates',
+                      'Hi, ${isLoggedIn ? "Nishant" : "Agriculturist!"}',
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 22,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Icon(
-                      Icons.arrow_forward_ios,
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      '23 Jan, 2021',
+                      style: TextStyle(
+                        color: kPrimarySwatch.shade100,
+                      ),
+                    )
+                  ],
+                ),
+                //Notification
+                Container(
+                  decoration: BoxDecoration(
+                    //color: kPrimarySwatch.shade100,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.all(12),
+                  child: isLoggedIn
+                      ? CircleAvatar()
+                      : IconButton(
+                          onPressed: () async {
+                            Navigator.pushReplacementNamed(context, '/login');
+                          },
+                          icon: const Icon(
+                            Icons.login,
+                            color: Colors.white,
+                          ),
+                        ),
+                )
+              ],
+            ),
+
+            SizedBox(
+              height: 28,
+            ),
+
+            //search bar
+            Container(
+              decoration: BoxDecoration(
+                color: kPrimarySwatch.shade100,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: EdgeInsets.all(12),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Search',
+                    style: TextStyle(
                       color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 25,
+            ),
+
+            // weather
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Weather Updates',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+
+            SizedBox(
+              height: 25,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    WeatherCard(
+                      weatherFace: '🌧️',
+                    ),
+                    Text(
+                      'Yesterday',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      'Rainy',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ],
                 ),
-
-                SizedBox(
-                  height: 25,
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                Column(
                   children: [
-                    Column(
-                      children: [
-                        WeatherCard(
-                          weatherFace: '🌧️',
-                        ),
-                        Text(
-                          'Yesterday',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Text(
-                          'Rainy',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
+                    WeatherCard(
+                      weatherFace: '🌧️',
                     ),
-                    Column(
-                      children: [
-                        WeatherCard(
-                          weatherFace: '🌧️',
-                        ),
-                        Text(
-                          'Today',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Text(
-                          'Rainy',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
+                    Text(
+                      'Today',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
                     ),
-                    Column(
-                      children: [
-                        WeatherCard(
-                          weatherFace: '🌧️',
-                        ),
-                        Text(
-                          'Tommorrow',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                        Text(
-                          'Rainy',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
+                    Text(
+                      'Rainy',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    WeatherCard(
+                      weatherFace: '🌧️',
+                    ),
+                    Text(
+                      'Tommorrow',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      'Rainy',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ],
                 ),
               ],
             ),
-          ),
+          ],
+        ),
+      ),
 
-          SizedBox(
-            height: 25,
-          ),
+      SizedBox(
+        height: 25,
+      ),
 
-          // weather
+      // weather
 
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(25),
-              color: Colors.grey[100],
-              child: Center(
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'MarketPlace',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
+      Expanded(
+        child: Container(
+          padding: EdgeInsets.all(25),
+          color: Colors.grey[100],
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'MarketPlace',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
                         ),
-                        Icon(Icons.arrow_forward_ios),
-                      ],
-                    ),
-                    spacer(height: 10),
-                    MarketTile(),
-                    MarketTile(),
-                    MarketTile(),
-                  ],
-                ),
+                      ),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                  spacer(height: 10),
+                  MarketTile(),
+                  MarketTile(),
+                  MarketTile(),
+                ],
               ),
             ),
           ),
-        ]),
+        ),
       ),
-    );
+    ]);
   }
 }
