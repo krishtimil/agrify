1b3ad68935dd2104b47ca6662735c27bb68c6b8e
krishtimil <30183049+krishtimil@users.noreply.github.com>
2022-11-03 21:39:46 +0545

    daas

--- agrify/lib/ui/screens/infobook/infomation_page.dart
+++ agrify/lib/ui/screens/infobook/infomation_page.dart
@@ -1,4 +1,5 @@
 import 'package:agrify/logic/controllers/auth_methods.dart';
+import 'package:agrify/ui/utilities/crops_dataset.dart';
 import 'package:flutter/material.dart';
 import 'package:beautiful_ui_components/beautiful_ui_components.dart';
 import '../../components/weather_card.dart';
@@ -12,6 +13,7 @@ class InfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map cropText = cropDAta;
     return Column(children: [
       // grettings row
       spacer(height: 20),
@@ -47,11 +49,50 @@ class InfoPage extends StatelessWidget {
 
       Expanded(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+          padding: EdgeInsets.symmetric(
+            horizontal: 25,
+            vertical: 15,
+          ),
           color: Colors.grey[100],
-          child: Container(),
+          child: Column(
+            children: [
+              Tile(
+                title: 'Potato',
+              ),
+              Tile(
+                title: 'Tomato',
+              ),
+            ],
+          ),
         ),
       ),
     ]);
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: ExpansionTile(
+        title: Text(title),
+        children: [
+          Text('Hello'),
+        ],
+      ),
+    );
   }
 }
--- agrify/lib/ui/utilities/crops_dataset.dart
+++ agrify/lib/ui/utilities/crops_dataset.dart
@@ -1,40 +1,60 @@
 Map cropDAta = {
-  'Potato':  {
+  'Potato': {
     'cropname': 'potato',
     'season': 'late summer',
-    'fertilizer': 'NPK ratio of 15-15-15 while planting. The fertilizer of 34-0-0 is essential after a month or two as it needs lots of nitrogen. Similarly, during last couple of month, fertilizer of 12-12-17 is the best choice.',
-    'procedure':[
+    'fertilizer':
+        'NPK ratio of 15-15-15 while planting. The fertilizer of 34-0-0 is essential after a month or two as it needs lots of nitrogen. Similarly, during last couple of month, fertilizer of 12-12-17 is the best choice.',
+    'procedure': [
       'Digging a hole.',
-'Obtaining the plant.',
-'Watering the seedling.'
-'Removing the plant from the pot.',
-'Placing the plant in the hole.',
-'Covering the roots with soil.',
-'Gently packing the soil around the tree.'
+      'Obtaining the plant.',
+      'Watering the seedling.'
+          'Removing the plant from the pot.',
+      'Placing the plant in the hole.',
+      'Covering the roots with soil.',
+      'Gently packing the soil around the tree.'
     ]
   },
   'Tomato': {
     'cropname': 'tomato',
     'season': 'summer - winter',
-    'fertilizer': 'plenty of nitrogen at first to aid leaf production.A soil test will determine the levels of nitrogen present. If the soil is higher in nitrogen, apply a balanced 10-10-10 fertilizer, but if it is lower, you may want to opt for a 10-5-5 fertilizer instead.',
-    'procedure': ['Plant your tomato plants deeper than they come in the pot, all the way up to the top few leaves.']
+    'fertilizer':
+        'plenty of nitrogen at first to aid leaf production.A soil test will determine the levels of nitrogen present. If the soil is higher in nitrogen, apply a balanced 10-10-10 fertilizer, but if it is lower, you may want to opt for a 10-5-5 fertilizer instead.',
+    'procedure': [
+      'Plant your tomato plants deeper than they come in the pot, all the way up to the top few leaves.'
+    ]
   },
-  'Cabbage':{
-    'cropname':'cabbage',
+  'Cabbage': {
+    'cropname': 'cabbage',
     'season': 'summer-winter',
-    'fertilizer': 'Cabbages are heavy feeders. The ideal N-P-K ratio is 1-1-1 (can be found commonly as 10-10-10 or 12-12-12).',
-    'procedure': ['Plant each at a distance of 2 feet apart', 'We can plant our cabbage seeds indoors in seedbeds or pots 6 to 8 weeks before the last spring frost.',]
+    'fertilizer':
+        'Cabbages are heavy feeders. The ideal N-P-K ratio is 1-1-1 (can be found commonly as 10-10-10 or 12-12-12).',
+    'procedure': [
+      'Plant each at a distance of 2 feet apart',
+      'We can plant our cabbage seeds indoors in seedbeds or pots 6 to 8 weeks before the last spring frost.',
+    ]
   },
-  'Radish':{
+  'Radish': {
     'cropname': 'radish',
-    'season':'mainly spring',
-    'fertilizer':'Apply about one pound (0.45 kg.) of 16-20-0 or 10-10-10 fertilizer per 100 square feet (9 sq. m.)',
-    'procedure':['Plant each at a distance of 2 feet apart', 'Direct-sow seeds outdoors about 1/2-inch deep and cover loosely with soil.', 'Space 1 inch apart in rows 12 inches apart.', 'Water seeds thoroughly, down to 6 inches deep. Sow another round of seeds every 10 days or so while weather is still cool for a continuous harvest of radishes in the late spring and early summer.']
+    'season': 'mainly spring',
+    'fertilizer':
+        'Apply about one pound (0.45 kg.) of 16-20-0 or 10-10-10 fertilizer per 100 square feet (9 sq. m.)',
+    'procedure': [
+      'Plant each at a distance of 2 feet apart',
+      'Direct-sow seeds outdoors about 1/2-inch deep and cover loosely with soil.',
+      'Space 1 inch apart in rows 12 inches apart.',
+      'Water seeds thoroughly, down to 6 inches deep. Sow another round of seeds every 10 days or so while weather is still cool for a continuous harvest of radishes in the late spring and early summer.'
+    ]
   },
-  'Squash':{
+  'Squash': {
     'cropname': 'squash',
-    'season':'summer or winter',
-    'fertilizer':'Squash have very extensive root systems and respond to thorough soil preparation and the application of 3 to 4 pounds of 10-10-10 fertilizer per 100 square feet.',
-    'procedure':['Summer squash are often planted 24-30 inches apart while winter squash are planted 30-48 inches apart.', 'Chop off the top and bottom of the squash.', 'Peel or slice away the skin. Chop through the \'neck\' of the squash to leave two pieces and slice both in half lengthways.', 'Use an ice cream scoop to scrape out the seeds. ']
+    'season': 'summer or winter',
+    'fertilizer':
+        'Squash have very extensive root systems and respond to thorough soil preparation and the application of 3 to 4 pounds of 10-10-10 fertilizer per 100 square feet.',
+    'procedure': [
+      'Summer squash are often planted 24-30 inches apart while winter squash are planted 30-48 inches apart.',
+      'Chop off the top and bottom of the squash.',
+      'Peel or slice away the skin. Chop through the \'neck\' of the squash to leave two pieces and slice both in half lengthways.',
+      'Use an ice cream scoop to scrape out the seeds. '
+    ]
   }
 };
